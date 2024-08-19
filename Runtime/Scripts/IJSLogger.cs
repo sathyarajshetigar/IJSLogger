b02f149eeae55f0f4b423c441d4fffa4a6b634d4
Sathyaraj Shettigar <42057975+sathyarajshetigar@users.noreply.github.com>
2024-08-19 15:26:40 +0530

    Remove unused files and add new Unity sample scene
    Removed various scripts, batch files, and documentation that are no longer in use. Added a new Unity sample scene for demonstrating Logger functionalities to improve usability and understanding. The new files include `.idea` configurations and a sample scene `LoggerExample.unity`.

--- Runtime/Scripts/IJSLogger.cs
+++ Runtime/Scripts/IJSLogger.cs
@@ -1,13 +1,62 @@
-using System;
-using System.Diagnostics;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
 using System.Runtime.CompilerServices;
+#endif
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace com.ijs.logger
 {
     public class IJSLogger
     {
+#if UNITY_EDITOR
+        private const string UseLogs = "USE_LOGS";
+        private static bool _useLogs;
+
+        [MenuItem("IJS/Logger/Enable Logs")]
+        private static void EnableUseLogs()
+        {
+            _useLogs = true;
+            OnUseLogsChanged();
+        }
+
+        [MenuItem("IJS/Logger/Disable Logs")]
+        private static void DisableUseLogs()
+        {
+            _useLogs = false;
+            OnUseLogsChanged();
+        }
+
+        private static void OnUseLogsChanged()
+        {
+            UpdateScriptingDefineSymbols(UseLogs, _useLogs);
+        }
+
+        private static void UpdateScriptingDefineSymbols(string val, bool add)
+        {
+#if UNITY_EDITOR
+        var platform = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+        var definesString = PlayerSettings.GetScriptingDefineSymbols(platform);
+        var allDefines = definesString.Split(';').ToList();
+        if (add)
+        {
+            if (!allDefines.Contains(val))
+                allDefines.Add(val);
+        }
+        else
+        {
+            if (allDefines.Contains(val))
+                allDefines.Remove(val);
+        }
+        PlayerSettings.SetScriptingDefineSymbols(platform, string.Join(";", allDefines.ToArray()));
+#endif
+        }
+#endif
+        
         private Color _logColor; // Color for log messages
         private bool _logsEnabled; // Whether or not to log
         private string _logPrefix; // Prefix for log messages
@@ -40,11 +89,6 @@ namespace com.ijs.logger
             if (!_logsEnabled) return;
             message = $"{_logPrefix}:: {message}";
             Log(message, logType, go, _logColor);
-        }
-
-        public static void LogError()
-        {
-
         }
 
         [Conditional("USE_LOGS")]
