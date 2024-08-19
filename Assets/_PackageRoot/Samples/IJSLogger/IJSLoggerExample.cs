12b138d0aaccf842fe673965ba63bd4d8e84c8a4
Sathyaraj Shettigar <42057975+sathyarajshetigar@users.noreply.github.com>
2024-08-19 16:08:13 +0530

    Add IJSLogger sample and update package path
    This commit introduces a new sample for IJSLogger, including the main script and associated meta files. Additionally, the path in the package.json file has been updated for proper sample location reference.

---
+++ Assets/_PackageRoot/Samples/IJSLogger/IJSLoggerExample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace com.ijs.logger.samples
+{
+    public class IJSLoggerExample : MonoBehaviour
+    {
+        #region Logger
+
+        private readonly IJSLogger _logger = new("IJSLoggerExample", Color.yellow);
+        [SerializeField] private bool _enableLogs;
+        [SerializeField] private Color _logColor;
+
+        private void OnValidate()
+        {
+            _logger.ToggleLogs(_enableLogs);
+            _logger.ModifyColor(_logColor);
+        }
+
+        #endregion
+
+        private void Awake()
+        {
+            IJSLogger.Log("IJSLoggerExample Log");
+            _logger.PrintLog("IJSLoggerExample PrintLog");
+        }
+    }
+}
