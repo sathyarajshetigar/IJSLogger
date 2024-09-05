fc4672b981b8d3ada28e2467fa405310e4979c6b
Sathyaraj Shettigar <42057975+sathyarajshetigar@users.noreply.github.com>
2024-09-05 16:30:06 +0530

    Update logger namespace and fix layout selections
    Changed the namespace from 'Utils' to 'com.ijs.logger' in IJSLogger.cs. Adjusted default-6000.dwlt to correct selected and expanded IDs. Updated package version from 1.0.9 to 1.0.10 in package.json.

--- Runtime/Scripts/IJSLogger.cs
+++ Runtime/Scripts/IJSLogger.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
-namespace Utils
+namespace com.ijs.logger
 {
     public class IJSLogger
     {
