e38075b0a848b7f40b0431a7536c91b6e51b57c0
Sathyaraj Shettigar <42057975+sathyarajshetigar@users.noreply.github.com>
2024-09-05 16:25:37 +0530

    Update logger implementation and UI layout settings
    Refactored `IJSLogger` to remove Unity Editor-specific code and improve stack trace functionality for better logging. Modified various layout parameters including titles, control IDs, and element positions. Updated version in `package.json` to 1.0.9.

--- Runtime/Scripts/IJSLogger.cs
+++ Runtime/Scripts/IJSLogger.cs
@@ -1,89 +1,19 @@
-#if UNITY_EDITOR
-using UnityEditor;
-using UnityEditor.Build;
-#endif
-using UnityEngine;
-using Debug = UnityEngine.Debug;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
 
-namespace com.ijs.logger
+namespace Utils
 {
-    /// <summary>
-    /// The <c>IJSLogger</c> class is a utility class that provides logging capabilities in Unity.
-    /// It allows you to print log messages with customizable prefixes, colors, and log types.
-    /// </summary>
     public class IJSLogger
     {
-#if UNITY_EDITOR
-        private const string UseLogs = "USE_LOGS";
-        private static bool useLogs;
-
-        /// <summary>
-        /// Enables or disables the use of logs in the IJSLogger class.
-        /// This method sets a scripting define symbol based on the enable parameter value.
-        /// </summary>
-        [MenuItem("IJS/Logger/Enable Logs")]
-        private static void EnableUseLogs()
-        {
-            useLogs = true;
-            OnUseLogsChanged();
-        }
-
-        /// <summary>
-        /// The <c>DisableUseLogs</c> method disables the use of logs in the Unity application.
-        /// </summary>
-        [MenuItem("IJS/Logger/Disable Logs")]
-        private static void DisableUseLogs()
-        {
-            useLogs = false;
-            OnUseLogsChanged();
-        }
-
-        /// <summary>
-        /// The <c>IJSLogger</c> class is a utility class that provides logging capabilities in Unity.
-        /// It allows you to print log messages with customizable prefixes, colors, and log types.
-        /// </summary>
-        private static void OnUseLogsChanged()
-        {
-            UpdateScriptingDefineSymbols(UseLogs, useLogs);
-        }
-
-
-        /// <summary>
-        /// Updates the scripting define symbols based on the given value and whether to add or remove it.
-        /// </summary>
-        /// <param name="val">The value to add or remove from the scripting defines symbols.</param>
-        /// <param name="add">A boolean indicating whether to add or remove the given value.</param>
-        private static void UpdateScriptingDefineSymbols(string val, bool add)
-        {
-#if UNITY_EDITOR
-        var platform = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-        var definesString = PlayerSettings.GetScriptingDefineSymbols(platform);
-        var allDefines = definesString.Split(';').ToList();
-        if (add)
-        {
-            if (!allDefines.Contains(val))
-                allDefines.Add(val);
-        }
-        else
-        {
-            if (allDefines.Contains(val))
-                allDefines.Remove(val);
-        }
-        PlayerSettings.SetScriptingDefineSymbols(platform, string.Join(";", allDefines.ToArray()));
-#endif
-        }
-#endif
-        
         private Color _logColor; // Color for log messages
-        private bool _logsEnabled; // Whether to log
+        private bool _logsEnabled; // Whether or not to log
         private string _logPrefix; // Prefix for log messages
 
-        /// The `IJSLogger` class is a utility class that provides logging capabilities in Unity.
-        /// It allows you to print log messages with customizable prefixes, colors, and log types.
-        /// /
         public IJSLogger(string prefix = "", Color? color = null, bool logsEnabled = true)
         {
             _logColor = color ?? Color.white;
@@ -91,44 +21,21 @@ namespace com.ijs.logger
             _logsEnabled = logsEnabled;
         }
 
-        /// <summary>
-        /// ToggleLogs method is used to enable or disable logging in IJSLogger class.
-        /// </summary>
-        /// <param name="enable">A boolean value indicating whether to enable or disable logging.</param>
         public void ToggleLogs(bool enable)
         {
             _logsEnabled = enable;
         }
 
-        /// <summary>
-        /// Modifies the prefix for log messages.
-        /// </summary>
-        /// <param name="prefix">The new prefix for log messages.</param>
         public void ModifyPrefix(string prefix)
         {
             _logPrefix = prefix;
         }
 
-        /// <summary>
-        /// Modifies the color used for log messages.
-        /// </summary>
-        /// <param name="color">The new color for log messages.</param>
         public void ModifyColor(Color color)
         {
             _logColor = color;
         }
 
-        /// <summary>
-        /// Prints a log message with customizable prefixes, colors, and log types.
-        /// </summary>
-        /// <param name="message">The log message to print.</param>
-        /// <param name="logType">The type of the log (LogType.Log by default).</param>
-        /// <param name="go">The GameObject associated with the log message (null by default).</param>
-        /// <remarks>
-        /// This method is used to print log messages with customized prefixes, colors, and log types.
-        /// The message is formatted with the specified prefix and then logged using Unity's Debug class.
-        /// If logs are not enabled, the method does nothing.
-        /// </remarks>
         [Conditional("USE_LOGS")]
         public void PrintLog(string message, LogType logType = LogType.Log, GameObject go = null)
         {
@@ -137,9 +44,6 @@ namespace com.ijs.logger
             Log(message, logType, go, _logColor);
         }
 
-        /// <summary>
-        /// Allows logging messages with customizable prefixes, colors, and log types.
-        /// </summary>
         [Conditional("USE_LOGS")]
         public static void Log(
             string message,
@@ -183,18 +87,76 @@ namespace com.ijs.logger
                     break;
             }
 #else
-            var stackTrace = new StackTrace();
+            var stackTrace = new StackTrace(true);
+            var callerFrames = GetRelevantStackFrames(stackTrace);
 
-            var firstCallerMemberName = stackTrace.GetFrame(1).GetMethod().Name;
-            // var firstCallerSourceLineNumber = firstCaller.GetFileLineNumber();
+            // Get the method two steps back in the call stack, effectively skipping logger methods
+            var firstCallerFrame = callerFrames.ElementAtOrDefault(0);
+            var secondCallerFrame = callerFrames.ElementAtOrDefault(1);
 
-            var secondCallerMemberName = stackTrace.GetFrame(2).GetMethod().Name;
-             // var thirdCallerMemberName = stackTrace.GetFrame(3).GetMethod().Name;
+            var firstCallerClassName = firstCallerFrame?.GetMethod()?.DeclaringType?.Name ?? "UnknownClass";
+            var firstCallerMethodName = firstCallerFrame?.GetMethod()?.Name ?? "UnknownMethod";
 
+            var secondCallerClassName = secondCallerFrame?.GetMethod()?.DeclaringType?.Name ?? "UnknownClass";
+            var secondCallerMethodName = secondCallerFrame?.GetMethod()?.Name ?? "UnknownMethod";
 
-        // Debug.Log($"\n\n{message} : => firstMethod: {firstCallerMemberName}:secondMethod {secondCallerMemberName}\n");
-        Debug.Log($"\n\n{message}");
+            string logMessage = message;
+
+            // Append current class and method only if they are not unknown
+            if (firstCallerClassName != "UnknownClass" && firstCallerMethodName != "UnknownMethod")
+                logMessage += $" ⇒ F: {firstCallerClassName}.{firstCallerMethodName}";
+
+            // Append previous class and method only if they are not unknown
+            if (secondCallerClassName != "UnknownClass" && secondCallerMethodName != "UnknownMethod")
+                logMessage += $", S: {secondCallerClassName}.{secondCallerMethodName}";
+
+            Debug.Log($"\n\n{logMessage}\n");
 #endif
+        }
+
+
+        private static List<StackFrame> GetRelevantStackFrames(StackTrace stackTrace)
+        {
+            var frames = new List<StackFrame>();
+            for (var i = 0; i < stackTrace.FrameCount; i++)
+            {
+                var frame = stackTrace.GetFrame(i);
+                var methodName = frame?.GetMethod()?.Name;
+                var className = frame?.GetMethod()?.DeclaringType?.Name;
+                if (methodName != "MoveNext"
+                    && methodName != "InvokeMoveNext"
+                    && !(methodName == "Start" && className == "AsyncVoidMethodBuilder")
+                    && methodName != "PrintLog"
+                    && methodName != "RunInternal"
+                    && methodName != "InvokeAction"
+                    && methodName != "FinishContinuations"
+                    && methodName != "Invoke"
+                    && methodName != "ExecuteTasks"
+                    && methodName != "RunCallback"
+                    && methodName != "Exec"
+                    && !(className?.StartsWith("AsyncTaskMethodBuilder") ?? false)
+                    && !(className == "ExecutionContext" && methodName == "Run")
+                    && !(className == "SynchronizationContextAwaitTaskContinuation" && methodName == "Run")
+                    && !(className == "AwaitTaskContinuation" && methodName == "InvokeAction")
+                    && !(className == "MoveNextRunner" && methodName == "Run")
+                    && className != "IJSLogger"
+                    && className != "Tween"
+                    && className != "Clickable"
+                    && className != "TaskCompletionSource"
+                    && className != "TweenManager"
+                    && className != "UnitySynchronizationContext"
+                    && className != "Task"
+                    && className != "InternalEditorUtility"
+                    && className != "DOTweenComponent"
+                    && className != "Socket"
+                    && className != "Task`1"
+                    && className != "<>c"
+                    && !(className == "WorkRequest" && methodName == "Invoke")) // Ignore specific methods and classes
+                {
+                    frames.Add(frame);
+                }
+            }
+            return frames;
         }
     }
 }
